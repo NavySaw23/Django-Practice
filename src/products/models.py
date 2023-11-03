fbb9a7f3ad7d8dfe0c5bb0364926160af98efb23
Navinya <Navinya Sawarkar>
2023-11-03 12:54:30 +0530

    initialized pages app

--- src/products/models.py
+++ src/products/models.py
@@ -3,11 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    title = models.TextField()
-    description = models.TextField()
+    title = models.CharField(max_length=100)                    #the max_length is mandatory for CharField
+    description = models.TextField(blank = True, null = True)   #if blank is false, blank values will not be allowed i.e. mandatory field
     price = models.TextField(default='100')
+    checkbox = models.BooleanField(null=False)                  #if null is true it will become a trinary instead of boolean with Yes, No and Unknown state
     
-    # Other possible options -
-    # id = models.IntegerField()
-    # link = models.URLField()
-    # upload = models.FileField()
+    # https://docs.djangoproject.com/en/4.2/ref/models/fields/#field-types  <-complete list
