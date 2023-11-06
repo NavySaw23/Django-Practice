3f00043308e4ee876daa59788a4c0f33266a9eee
Navinya <Navinya Sawarkar>
2023-11-06 12:57:00 +0530

    Templating modules

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,6 +1,18 @@
-from django.http import HttpResponse
+from django.http import HttpResponse #makes the html functional
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1> hello world </h1>")
+def home_view(request, *args, **kwargs):
+    print(request)
+    print(request.user)    #request.user is used for authentication and stuff
+    print(args)
+    print(kwargs)
+    #above 3 lines show what request is being made
+    return HttpResponse("<h1> Hello world </h1>") 
+
+def contact_view(*args, **kwargs):
+    return HttpResponse("<h1> Contacts page </h1>") 
+
+def template_view(request, *args, **kwargs):
+    return render(request, "template.html", {})
+
